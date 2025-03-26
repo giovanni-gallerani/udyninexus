85893d9c10bcb12699d8f74cac8737b18709bc44
giovanni-gallerani <giovanni.gallerani@hotmail.it>
2025-03-26 03:01:15 +0100

    changed logging settings level to warning

--- udyninexus/logging_settings.py
+++ udyninexus/logging_settings.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.WARNING,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
         # logging.FileHandler("app.log"),  # Save logs to a file
