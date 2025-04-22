2ac8dcdeb08165bf08baafea3af70df2e88392f5
giovanni-gallerani <giovanni.gallerani@hotmail.it>
2025-04-22 23:08:47 +0200

    removed defaul messages from custom exceptions

--- udyninexus/exceptions.py
+++ udyninexus/exceptions.py
@@ -1,10 +1,7 @@
 class NexusValidationError(Exception):
-    """Raised by write_nexus when the NexusDataContainer fails validation."""
-    def __init__(self, message="Invalid NexusDataContainer, the NeXus file cannot be created"):
-        super().__init__(message)
-
+    """Raised by write_nexus when the NexusContainer fails validation."""
+    pass
 
 class NexusSaveError(Exception):
     """Raised by write_nexus when saving to file fails unexpectedly."""
-    def __init__(self, message="An error occoured while trying to save the file"):
-        super().__init__(message)
+    pass
