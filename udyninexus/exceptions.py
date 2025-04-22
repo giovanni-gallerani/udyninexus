0c43297159b63b432540ccd8a92bc6b68ef28595
giovanni-gallerani <giovanni.gallerani@hotmail.it>
2025-04-22 23:09:41 +0200

    made the comments more general

--- udyninexus/exceptions.py
+++ udyninexus/exceptions.py
@@ -1,7 +1,7 @@
 class NexusValidationError(Exception):
-    """Raised by write_nexus when the NexusContainer fails validation."""
+    """Raised when the NexusContainer fails validation."""
     pass
 
 class NexusSaveError(Exception):
-    """Raised by write_nexus when saving to file fails unexpectedly."""
+    """Raised when saving to file fails unexpectedly."""
     pass
