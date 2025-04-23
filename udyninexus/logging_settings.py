94aad883cd6df8bd5d8e990f2302f92f1602d593
giovanni-gallerani <giovanni.gallerani@hotmail.it>
2025-04-23 23:56:40 +0200

    changed logging settings to INFO

--- udyninexus/logging_settings.py
+++ udyninexus/logging_settings.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-    level=logging.WARNING,
+    level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
         # logging.FileHandler("app.log"),  # Save logs to a file
